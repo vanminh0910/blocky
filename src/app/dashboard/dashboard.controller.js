4ce7e202b7485cae1f39219794bfcdb725ee92be
TMA\nbxtruong <omega220@gmail.com>
2017-10-13 17:54:45 +0700

    [Fix bugs] Disable movement for gmap widget on edit mode

--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -249,6 +249,14 @@ export default function DashboardController($scope, userService, dashboardServic
     }
 
     function editDashboard() {
+        var content = vm.currentDashboard.content;
+        for (var i = 0; i < vm.currentDashboard.content.length; i++)
+            if (content[i].type == 'gmap' && content[i].mapObject) {
+                content[i].mapObject.setOptions({
+                    draggable: false
+                });
+            }
+
         vm.editMode = true;
         vm.gridsterOptions.draggable.enabled = true;
         vm.gridsterOptions.resizable.enabled = true;
@@ -304,6 +312,14 @@ export default function DashboardController($scope, userService, dashboardServic
     }
 
     function runDashboard() {
+        var content = vm.currentDashboard.content;
+        for (var i = 0; i < vm.currentDashboard.content.length; i++)
+            if (content[i].type == 'gmap' && content[i].mapObject) {
+                content[i].mapObject.setOptions({
+                    draggable: true
+                });
+            }
+
         vm.editMode = false;
         vm.gridsterOptions.draggable.enabled = false;
         vm.gridsterOptions.resizable.enabled = false;
@@ -815,7 +831,14 @@ export default function DashboardController($scope, userService, dashboardServic
     }
 
     function initMap(coordinates, id) {
-        vm.map = new google.maps.Map(document.getElementById(id), {
+        var thisMap;
+        var content = vm.currentDashboard.content;
+        for (var i = 0; i < content.length; i++)
+            if (content[i].type == 'gmap' && content[i].id == id) {
+                thisMap = content[i];
+                break;
+            }
+        thisMap.mapObject = vm.map = new google.maps.Map(document.getElementById(id), {
             center: coordinates,
             zoom: 15,
         });
@@ -827,7 +850,14 @@ export default function DashboardController($scope, userService, dashboardServic
     }
 
     function polylineMap(coordinates, id) {
-        vm.map = new google.maps.Map(document.getElementById(id), {
+        var thisMap;
+        var content = vm.currentDashboard.content;
+        for (var i = 0; i < content.length; i++)
+            if (content[i].type == 'gmap' && content[i].id == id) {
+                thisMap = content[i];
+                break;
+            }
+        thisMap.mapObject = vm.map = new google.maps.Map(document.getElementById(id), {
             zoom: 3,
             center: coordinates[coordinates.length - 1],
             mapTypeId: 'terrain',
