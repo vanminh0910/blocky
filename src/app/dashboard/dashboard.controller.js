b77635dcf2f521f8d8f9be6e5069617ed2dd542d
Nguyen Bao Xuan Truong <omega220@gmail.com>
2017-10-14 11:36:08 +0700

    [Fix bugs] Disable map movement on edit mode

--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -252,13 +252,15 @@ export default function DashboardController($scope, userService, dashboardServic
     }
 
     function editDashboard() {
-        // var content = vm.currentDashboard.content;
-        // for (var i = 0; i < vm.currentDashboard.content.length; i++)
-        //     if (content[i].type == 'gmap' && content[i].mapObject) {
-        //         content[i].mapObject.setOptions({
-        //             draggable: false
-        //         });
-        //     }
+        var content = vm.currentDashboard.content;
+        $log.log(content);
+        for (var i = 0; i < vm.currentDashboard.content.length; i++) {
+            if (content[i].type == 'gmap' && content[i].mapObject) {
+                content[i].mapObject.setOptions({
+                    draggable: false
+                });
+            }
+        }
 
         vm.editMode = true;
         vm.gridsterOptions.draggable.enabled = true;
@@ -315,13 +317,14 @@ export default function DashboardController($scope, userService, dashboardServic
     }
 
     function runDashboard() {
-        // var content = vm.currentDashboard.content;
-        // for (var i = 0; i < vm.currentDashboard.content.length; i++)
-        //     if (content[i].type == 'gmap' && content[i].mapObject) {
-        //         content[i].mapObject.setOptions({
-        //             draggable: true
-        //         });
-        //     }
+        var content = vm.currentDashboard.content;
+        for (var i = 0; i < vm.currentDashboard.content.length; i++) {
+            if (content[i].type == 'gmap' && content[i].mapObject) {
+                content[i].mapObject.setOptions({
+                    draggable: true
+                });
+            }
+        }
 
         vm.editMode = false;
         vm.gridsterOptions.draggable.enabled = false;
@@ -837,15 +840,17 @@ export default function DashboardController($scope, userService, dashboardServic
     }
 
     function initMap(coordinates, id) {
-        // var thisMap;
-        // var content = vm.currentDashboard.content;
-        // for (var i = 0; i < content.length; i++)
-        //     if (content[i].type == 'gmap' && content[i].id == id) {
-        //         thisMap = content[i];
-        //         break;
-        //     }
-        // thisMap.mapObject = vm.map = new google.maps.Map(document.getElementById(id), {
-            vm.map = new google.maps.Map(document.getElementById(id), {
+        var thisMap;
+        var content = vm.currentDashboard.content;
+        for (var i = 0; i < content.length; i++) {
+            if (content[i].type == 'gmap' && content[i].id == id) {
+                thisMap = content[i];
+                break;
+            }
+        }
+
+        thisMap.mapObject = vm.map = new google.maps.Map(document.getElementById(id), {
+            // vm.map = new google.maps.Map(document.getElementById(id), {
             center: coordinates,
             zoom: 15,
         });
@@ -857,15 +862,17 @@ export default function DashboardController($scope, userService, dashboardServic
     }
 
     function polylineMap(coordinates, id) {
-        // var thisMap;
-        // var content = vm.currentDashboard.content;
-        // for (var i = 0; i < content.length; i++)
-        //     if (content[i].type == 'gmap' && content[i].id == id) {
-        //         thisMap = content[i];
-        //         break;
-        //     }
-        // thisMap.mapObject = vm.map = new google.maps.Map(document.getElementById(id), {
-            vm.map = new google.maps.Map(document.getElementById(id), {
+        var thisMap;
+        var content = vm.currentDashboard.content;
+        for (var i = 0; i < content.length; i++) {
+            if (content[i].type == 'gmap' && content[i].id == id) {
+                thisMap = content[i];
+                break;
+            }
+        }
+
+        thisMap.mapObject = vm.map = new google.maps.Map(document.getElementById(id), {
+            // vm.map = new google.maps.Map(document.getElementById(id), {
             zoom: 3,
             center: coordinates[coordinates.length - 1],
             mapTypeId: 'terrain',
